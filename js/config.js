1fb4c6fbaca5bca2294a7547f259dbf47b32dc87
breezefeel <77193326+breezefeel@users.noreply.github.com>
2025-12-11 15:40:13 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,11 +2,11 @@
 
 // Google Apps Script 웹 앱 URL을 여기에 입력하세요
 const CONFIG = {
-    APPS_SCRIPT_URL: 'YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE',
+    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwAxEYwSbtFc1li0_BR-Ubnh_DaiIuwBwyvEC2NQlzXx0oZQ3_HFTdri6MYowMTn1wadg/exec',
     // 예시: 'https://script.google.com/macros/s/AKfycbx.../exec'
 };
 
 // Apps Script URL이 설정되지 않았을 때 경고
-if (CONFIG.APPS_SCRIPT_URL === 'YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE') {
+if (CONFIG.APPS_SCRIPT_URL === 'https://script.google.com/macros/s/AKfycbwAxEYwSbtFc1li0_BR-Ubnh_DaiIuwBwyvEC2NQlzXx0oZQ3_HFTdri6MYowMTn1wadg/exec') {
     console.warn('⚠️ Google Apps Script URL이 설정되지 않았습니다. js/config.js 파일을 수정해주세요.');
 }
